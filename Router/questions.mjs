3033dfaea574bd108cf088a9db044f38f33865fe
nantasit <nantasitnsboss@gmail.com>
2025-04-11 10:44:02 +0700

    feat :Add Get questionId questuin endpoint

--- Router/questions.mjs
+++ Router/questions.mjs
@@ -22,4 +22,18 @@ questionsRouter.get("/",[],async (req, res) => {
     return res.status(200).json({date:result.rows});
     }catch(e){ return res.status(500).json({"message": "Unable to fetch questions."})}
   });
-  export default questionsRouter
+
+questionsRouter.get("/:questionId",[],async (req, res) => {
+    const id = req.params.questionId
+    try{
+      const result = await connectionPool.query(
+        'select * from questions where id=$1 ',[id])
+      if(result.rows.length === 0){return res.status(404).json({"message": "Question not found."});} //สงสัย ? ถ้าไปเช็คใน Middleware ก็ต้อง req ไปหา server 2 รอบหรอ?
+      return res.status(200).json({date:result.rows[0]});
+      }catch(e){ return res.status(500).json({"message": "Unable to fetch questions."})}
+    });
+
+
+
+export default questionsRouter
+
