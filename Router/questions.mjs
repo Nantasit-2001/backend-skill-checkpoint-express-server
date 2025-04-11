f1d0189a651bafc97886ec532326b68848c56432
nantasit <nantasitnsboss@gmail.com>
2025-04-11 10:25:25 +0700

    feat:Add POST question endpoint And Create middleware

--- Router/questions.mjs
+++ Router/questions.mjs
@@ -1,7 +1,18 @@
 import { Router } from "express";
 import connectionPool from '../utils/db.mjs'
 
+import { ValidateCreateQuestion } from "../middleware/questionValidation.js";
+
 const questionsRouter=Router();
+
+questionsRouter.post("/",[ValidateCreateQuestion], async (req,res)=>{
+  const {title, description,category}=req.body
+  try{
+    const result = await connectionPool.query(
+      'INSERT INTO questions (title,description,category) VALUES($1,$2,$3)',[title,description,category])
+      return res.status(201).json({"message": "Question created successfully."})
+    }catch(e){return res.status(500).json({"message": "Unable to create question."})}
+}); 
 
 questionsRouter.get("/",[],async (req, res) => {
   try{
---
+++ middleware/questionValidation.js
@@ -0,0 +1,7 @@
+export const ValidateCreateQuestion = (req,res,next)=>{
+    const {title, description,category}=req.body
+    if(!title || !description ||!category ){
+        return res.status(400).json( {"message": "Invalid request data."})
+    }
+    next();
+}
