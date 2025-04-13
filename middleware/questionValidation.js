cf8a33ec402397850c3b30fac405ecfe39fec4dd
nantasit <nantasitnsboss@gmail.com>
2025-04-13 09:38:41 +0700

    fix(questionValidation):Add condition  content cannot exceed 300 char in checkEmptyBodyAnswers .

--- middleware/questionValidation.js
+++ middleware/questionValidation.js
@@ -8,7 +8,7 @@ export const checkEmptyBodyQuestion = (req,res,next)=>{
 
 export const checkEmptyBodyAnswers = (req,res,next) =>{
     const {content} = req.body
-    if(!content){
+    if(!content ||content.length>300){
         return res.status(400).json({"message": "Invalid request data."})}
     next();
 }
